a71829405994ab0b523320b352ba1cdf65add0f0
Ruona Dibie <ogheneruona.dibie@gmail.com>
2019-10-09 23:49:39 -0700

    Updates

---
+++ precache-manifest.6d104f628d19df21d1a5ca32fdde1c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897aee0c2cc1aceef6fff9ea14f9681b",
+    "url": "/ogdibie/inspire-logo-puzzle/index.html"
+  },
+  {
+    "revision": "6ebbffad6f2ea1ce158f",
+    "url": "/ogdibie/inspire-logo-puzzle/static/css/main.7aedf619.chunk.css"
+  },
+  {
+    "revision": "58305e90afeed91694fe",
+    "url": "/ogdibie/inspire-logo-puzzle/static/js/2.540a9215.chunk.js"
+  },
+  {
+    "revision": "6ebbffad6f2ea1ce158f",
+    "url": "/ogdibie/inspire-logo-puzzle/static/js/main.0c761fcf.chunk.js"
+  },
+  {
+    "revision": "d0acb73352b11a22b60a",
+    "url": "/ogdibie/inspire-logo-puzzle/static/js/runtime-main.9a44920d.js"
+  },
+  {
+    "revision": "b07ca556386c934cd23ea0b66bafb046",
+    "url": "/ogdibie/inspire-logo-puzzle/static/media/ia-logo-back.b07ca556.png"
+  }
+]);
